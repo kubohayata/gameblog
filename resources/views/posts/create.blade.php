1f8cfeaaaf8195ba26e91aefc0e32a180efff4a1
kubohayata <azite723@gmail.com>
2023-03-20 02:02:03 +0900

    成処理実行時のバリデーション実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,12 +13,14 @@
        <form action="/posts" method="POST">
            @csrf
            <div class="title",
-               <h2>Title</h2>
-               <input type="text" name=post[title] placeholder="ゲームタイトル">
+               <h2>タイトル</h2>
+               <input type="text" name=post[title] placeholder="ゲームタイトル" value={{ old('post.title' )}}>
+               <p class='title__error' style="color:red">{{ $errors->first('post.title') }} </p>
            </div>
            <div class="body">
-               <h2>body</h2>
-               <textarea name="post[body]" placeholder="攻略内容"></textarea>
+               <h2>内容</h2>
+               <textarea name="post[body]" placeholder="攻略内容">{{old('post.body')}}</textarea>
+               <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
            </div>
            <input type="submit" value="投稿">
        </form>
