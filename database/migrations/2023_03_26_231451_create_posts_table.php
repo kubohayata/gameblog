85edda8f043f5447b4767b6c5d71a92fa03fa15f
kubohayata <azite723@gmail.com>
2023-03-26 23:23:52 +0900

    マイグレーション

--- database/migrations/2023_03_26_231451_create_posts_table.php
+++ database/migrations/2023_03_26_231451_create_posts_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50);
+            $table->string('title', 50);
+            $table->string('body', 200);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -27,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 };
